53c79279140a33afab50e0b43f7674fad344592d
Chris I <chrisxesq@gmail.com>
2022-06-19 20:56:31 -0400

    socketio connection & test api calls

--- client/App.jsx
+++ client/App.jsx
@@ -2,28 +2,38 @@ import React, { useEffect } from 'react';
 // import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+import socket from "./socket"
 
 const App = () => {
 
+  const connectIO = () => {
+    socket.connect(); 
 
+    socket.on("kafka_controller_controllerstats_autoleaderbalancerateandtimems",(data)=>{
+      console.log('incoming message: ',data)
+    })
+  }
   return (
-    <Router>
-        <nav className="navbar">
-            <div className="navbar-links"></div>
-        </nav>
-        <Routes>
-            {/* <Route path="/login" element={ <Login /> }/> 
-            <Route path="/journey" element={ <Journey /> }/> 
-            <Route path="/profile" element={ <Profile /> }/> */}
+    <>
+    <label>Kafka IP:<input type="text" name="name" required/></label>
+    <button onClick={connectIO}>connect</button>
+     {/* <Router> */}
+        {/* <nav className="navbar"> */}
+            {/* <div className="navbar-links"></div> */}
+        {/* </nav> */}
+        {/* <Routes> */}
+            {/* <Route path="/login" element={ <Login /> }/>  */}
+            {/* <Route path="/journey" element={ <Journey /> }/>  */}
+            {/* <Route path="/profile" element={ <Profile /> }/>  */}
             {/* <Route path="/*" element={<Landing />}/>  */}
             {/* error page */}
             {/* <Route path="*" element={<ErrorPage />}/>  */}
-        </Routes>
-    </Router>
+        {/* </Routes>  */}
+    {/* </Router> */}
+    </>
 
 
-  );
+  )
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,53 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const axios = require('axios');
 const app = express();
 const PORT = 3000;
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:8080",
+  },
+});
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 
+//what metrics do we need and how often do we have to pull it from Prometheus?
+
+io.on('connection', socket => {
+  console.log('client connected');  
+  
+  let data;
+  
+  function test(){
+    axios.get('http://localhost:9090/api/v1/query',{ 
+      params: {
+        query: 'kafka_controller_controllerstats_autoleaderbalancerateandtimems'
+      }})
+    .then(res => res.data)
+    .then(res => {
+      const array = [];
+      res.data.result.forEach(x=>array.push(x.value));
+      data = array;
+      socket.emit("kafka_controller_controllerstats_autoleaderbalancerateandtimems", 
+      data);
+    })
+    .catch(err => console.log(err.code))
+  }
+  setInterval(test, 5000);
+})
 
 
 app.get('/', (req, res) => {
+  console.log('connect')
   return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.get('/api/test', (req, res) => {
+  console.log('receiving get request from the frontend');
+  return res.status(200).json('get request worked');
 });
 
 
@@ -26,6 +63,8 @@ app.use((err, req, res, next) => {
   res.status(500).json(errObj.error);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+httpServer.listen(PORT, ()=>{
+  console.log(`Server listening on port: ${PORT}`)
 });
+
+
