07a97b2538f9999bd4dab01d6c39a925b5a7bbee
Chris I <chrisxesq@gmail.com>
2022-06-20 20:15:10 -0400

    taking in ip by socket.emit

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const {query, queries} = require('./queries');
 const app = express();
 const PORT = 3000;
-const httpServer = require("http").createServer();
+const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
     origin: "http://localhost:8080",
@@ -15,22 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 
+
 io.on('connection', socket => {
   console.log('client connected');  
-  setInterval(query, 5000, socket);
-
+  socket.on("ip", ip => {
+    setInterval(query, 5000, socket, ip);
+  })
 })
-
 app.get('/', (req, res) => {
   console.log('connect')
   return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
 });
-
-app.get('/api/test', (req, res) => {
-  console.log('receiving get request from the frontend');
-  return res.status(200).json('get request worked');
-});
-
 
 app.use('*', (req,res) => {
   res.status(404).send('Not Found - 404 handler in server.js');
--- server/timestamp.js
+++ server/timestamp.js
@@ -1,15 +1,15 @@
 function unixTimeStamptoTime(unix_timestamp){
   // Create a new JavaScript Date object based on the timestamp
   // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-  var date = new Date(unix_timestamp * 1000);
+  const date = new Date(unix_timestamp * 1000);
   // Hours part from the timestamp
-  var hours = date.getHours();
+  const hours = date.getHours();
   // Minutes part from the timestamp
-  var minutes = "0" + date.getMinutes();
+  const minutes = "0" + date.getMinutes();
   // Seconds part from the timestamp
-  var seconds = "0" + date.getSeconds();
+  const seconds = "0" + date.getSeconds();
   // Will display time in 10:30:23 format
-  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  const formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
   
   return formattedTime;
 }
