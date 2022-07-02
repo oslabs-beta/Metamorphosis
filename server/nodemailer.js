732e10d2087065b14507cb0e36b554a1ed0389ba
Chris I <chrisxesq@gmail.com>
2022-07-02 15:25:13 -0400

    mailer works now, need to connect w frontend

--- server/nodemailer.js
+++ server/nodemailer.js
@@ -4,34 +4,10 @@ require('dotenv').config();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-let mailConfig;
-if (process.env.NODE_ENV === 'production' ){
-  
-    // all emails are delivered to destination
-    mailConfig = {
-        host: 'smtp.sendgrid.net',
-        port: 587,
-        auth: {
-            user: 'real.user',
-            pass: 'verysecret'
-        }
-    };
-} else {
-    // all emails are catched by ethereal.email
-    mailConfig = {
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: 'sonia.schinner85@ethereal.email',
-            pass: 'xMfxcFCCC43m9JSzH8'
-        }
-    };
-}
-let transporter = nodemailer.createTransport(mailConfig);
-
 const messageCreator = (input) => {
   const defaultO = {
     to: 'sonia.schinner85@ethereal.email',
+    from: 'metamorphosisny33@gmail.com',
     subject: `${input} + ${Date.now()}`,
     text: 'please check your Kafka\'s health'
   };
@@ -39,35 +15,7 @@ const messageCreator = (input) => {
   return ans;
 }
 
-const callTransporter = (message) => {
-  
-  transporter.sendMail(messageCreator(message), (err, info)=>{
-    if(err){
-      console.log('nodemailer error');
-      return process.exit(1);
-    }
-    console.log('success!');
-    console.log(nodemailer.getTestMessageUrl(info));
-    transporter.close();
-  })
-}
-const callSendGrid = (x) => {
-  sgMail
-  .send({
-    to: 'chrisxesq@gmail.com', // Change to your recipient
-  from: 'metamorphosisny33@gmail.com', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  })
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error('errrrrr',error)
-  })
-}
-
-const throttle = (func, delay) => {
+function throttle(func, delay){
   let lastAlert = null;
   const throttled = (...args) => {
     let now = new Date().getTime();
@@ -85,9 +33,53 @@ const throttle = (func, delay) => {
   }
   return throttled;
 }
+// console.log('check processenv',process.env.NODE_ENV);
+let throttled_callTransport;
+if(process.env.NODE_ENV=='production'){
+  const callSendGrid = (message) => {
+    sgMail
+    .send(messageCreator(message))
+    .then(() => {
+      console.log('Email sent')
+    })
+    .catch((error) => {
+      console.error('errrrrr',error)
+    })
+  }
+  
+  throttled_callTransport = throttle(callSendGrid,1000*60*5);
 
-//const throttled_callTransport = throttle(callTransporter,1000*60*5);
+} else {
+  let  mailConfig = {
+          host: 'smtp.ethereal.email',
+          port: 587,
+          auth: {
+              user: 'sonia.schinner85@ethereal.email',
+              pass: 'xMfxcFCCC43m9JSzH8'
+          }
+      };
+  
+  let transporter = nodemailer.createTransport(mailConfig);
+  
+  
+  const callTransporter = (message) => {
+    
+    transporter.sendMail(messageCreator(message), (err, info)=>{
+      if(err){
+        console.log('nodemailer error');
+        return process.exit(1);
+      }
+      console.log('success!');
+      console.log(nodemailer.getTestMessageUrl(info));
+      transporter.close();
+    })
+  }
+  throttled_callTransport = throttle(callTransporter,1000*60*5);
 
-const throttled_callTransport = throttle(callSendGrid,1000*60*5);
+}
+
+/////////////////////////////////////////////////////////for dev environment
+
+////////////////////////////////////////////////////////////////
 
 module.exports = { throttled_callTransport, messageCreator };
