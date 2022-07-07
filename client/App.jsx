597e6172664cbd4dc88e82bc4ac96a31e380c58d
chenjosephine <jchen1114@gmail.com>
2022-07-06 20:11:58 -0400

    rebuilt dist

--- client/App.jsx
+++ client/App.jsx
@@ -6,9 +6,8 @@ import Connection from './ConnectionPage/Connection'
 import Sidebar from './Sidebar/Sidebar';
 import Broker from './BrokerDashboard/Broker';
 import Login from './LoginPage/login'
-import Consumer from './ConsumerDashboard/Consumer';
 import Producer from './ProducerDashboard/Producer';
-
+import Consumer from './ConsumerDashBoard/Consumer';
 
 
 import main from './scss/main.scss';
